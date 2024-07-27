be32227a28e695e47c1108e51ade695849b6d661
Solomon Yahwin <solomonyahwin@gmail.com>
2024-07-27 22:17:41 +0100

    First commit to app repo

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="L-div">
+        <img src="/images/logo.png" alt="logo-img" />
+        <h2>ReactFacts</h2>
+      </div>
+      <div className="R-div">
+        <h3>React Course - Project 1</h3>
+      </div>
+    </nav>
+  );
+}
